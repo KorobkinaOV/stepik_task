20bdaafa65e876857fb6b5e87cbde9789fd763cd
Korobkina Oksana <o.korobkina@mts-link.ru>
2025-05-17 20:37:03 +0200

    added styles for radio btns

---
+++ tests/integration/helpers/call-fn-test.js
@@ -0,0 +1,17 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'stepik-task/tests/helpers';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Helper | callFn', function (hooks) {
+  setupRenderingTest(hooks);
+
+  // TODO: Replace this with your real tests.
+  test('it renders', async function (assert) {
+    this.set('inputValue', '1234');
+
+    await render(hbs`{{call-fn this.inputValue}}`);
+
+    assert.dom().hasText('1234');
+  });
+});
