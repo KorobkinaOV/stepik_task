d369025c02e3885f65c3f9f8aa530a37e54e33f5
Korobkina Oksana <o.korobkina@mts-link.ru>
2025-05-18 17:37:29 +0200

    added submit logic and checkbox quiz

--- app/controllers/choice-quiz.js
+++ app/controllers/choice-quiz.js
@@ -1,21 +1,67 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { task } from 'ember-concurrency';
+import { checkAnswers } from '../utils/check-answers';
 
-export default class FruitController extends Controller {
-  selectedFruit = '';
-  fruits = [
-    { id: 'apple', label: 'Apple' },
-    { id: 'banana', label: 'Banana' },
-    { id: 'orange', label: 'Orange' },
+export default class ChoiceQuiz extends Controller {
+  @tracked result = null;
+
+  userAnswers = {};
+
+  firstQuestionOptions = [
+    { label: 'Apple', type: 'singleOption', question: 'firstQuestion' },
+    { label: 'Banana', type: 'singleOption', question: 'firstQuestion' },
+    { label: 'Orange', type: 'singleOption', question: 'firstQuestion' },
   ];
 
+  secondQuestionOptions = [
+    { label: 'Cat', type: 'multipleOption', question: 'secondQuestion' },
+    { label: 'Dog', type: 'multipleOption', question: 'secondQuestion' },
+    { label: 'Elephant', type: 'multipleOption', question: 'secondQuestion' },
+    { label: 'Wolf', type: 'multipleOption', question: 'secondQuestion' },
+  ];
+
+  correctFirstQuestionAnswers = 'Banana';
+  correctSecondQuestionAnswers = ['Dog', 'Cat'];
+
   @action
-  updateFruit(value) {
-    this.selectedFruit = value;
+  updateQuestion(label, type, question) {
+    if (type === 'singleOption') {
+      this.userAnswers[question] = label;
+      return;
+    }
+
+    if (type === 'multipleOption') {
+      if (
+        Array.isArray(this.userAnswers[question]) &&
+        this.userAnswers[question].includes(label)
+      ) {
+        this.userAnswers[question] = this.userAnswers[question].filter(
+          (answer) => answer !== label,
+        );
+      } else {
+        this.userAnswers[question] = this.userAnswers[question]
+          ? [...this.userAnswers[question], label]
+          : [label];
+      }
+      return;
+    }
+  }
+
+  @task
+  *checkAnswerTask() {
+    yield new Promise((resolve) => setTimeout(resolve, 2000)); // задержка
+
+    this.result = checkAnswers(
+      this.userAnswers,
+      this.correctFirstQuestionAnswers,
+      this.correctSecondQuestionAnswers,
+    );
   }
 
   @action
-  sendSelectedFruit() {
-    console.log('Selected fruit:', this.selectedFruit.target.value);
+  submitResult() {
+    this.checkAnswerTask.perform();
   }
 }
